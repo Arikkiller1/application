c2841388642f831dd03834bcd4c4c141d2b94370
Arikkiller1 <128838670+Arikkiller1@users.noreply.github.com>
2023-04-08 16:58:01 +0500

    Update app.py

--- app.py
+++ app.py
@@ -203,120 +203,121 @@ date_list = [current_date + timedelta(days=x) for x in range(day_input)]
 
 # Create a DataFrame to store the predictions
 predictions_df = pd.DataFrame(columns=['Day', 'Month', 'Year', 'Category', 'Item', 'PricePointName', 'Qty'])
-
-# Loop through the dates and make predictions for each item and price point
-for date in date_list:
-    for category in df['Category'].unique():
-        for item in df[df['Category'] == category]['Item'].unique():
-            for price_point in df[(df['Category'] == category) & (df['Item'] == item)]['PricePointName'].unique():
-                
-                # Create a row of data to pass to the model
-                row = {
-                    'Day': day_input,
-                    'Month': month_input,
-                    'Year': year_input,
-                    'Category': category,
-                    'Item': item,
-                    'PricePointName': price_point,
-                    # 'Size': df1[(df1['Category'] == category) & (df1['Item'] == item) & (df1['PricePointName'] == price_point)]['Size'].iloc[0]
-                }
-                
-                # Make a prediction using your trained model
-
-                prediction = model_ex.predict([list(row.values())])[0]
-                
-                # Add the prediction to the DataFrame
-                predictions_df = predictions_df.append({
-                    'Day': row['Day'],
-                    'Month': row['Month'],
-                    'Year': row['Year'],
-                    'Category': row['Category'],
-                    'Item': row['Item'],
-                    'PricePointName': row['PricePointName'],
-                    'Qty':prediction
-                }, ignore_index=True)
-
-# Check the predictions
-# predictions_df['Day'].describe()
-
-category_df= pd.read_csv("updated_Category.csv")
-
-
-item_df = pd.read_csv("updated_Item_labelencoding.csv")
-
-
-Pricepointname_df = pd.read_csv("updated_size_labelencoding.csv")
-
-
-# convert predictions_df into dataFrame
-predictions_df = pd.DataFrame(predictions_df)
-
+if st.button("predict"):
     
-# Category_Original 
-# change places of number and valuies column with each other
-# Load the label encoding CSV file into a dictionary object
-label_encoding = {}
-with open('updated_Category.csv', mode='r') as csv_file:
-    reader = csv.reader(csv_file)
-    for row in reader:
-        label_encoding[int(row[1])] = row[0]
+    # Loop through the dates and make predictions for each item and price point
+    for date in date_list:
+        for category in df['Category'].unique():
+            for item in df[df['Category'] == category]['Item'].unique():
+                for price_point in df[(df['Category'] == category) & (df['Item'] == item)]['PricePointName'].unique():
 
-# Convert the encoded values back to the original categorical values
-encoded_values = (predictions_df["Category"])
-original_values1 = [label_encoding[encoded_value] for encoded_value in encoded_values]
+                    # Create a row of data to pass to the model
+                    row = {
+                        'Day': day_input,
+                        'Month': month_input,
+                        'Year': year_input,
+                        'Category': category,
+                        'Item': item,
+                        'PricePointName': price_point,
+                        # 'Size': df1[(df1['Category'] == category) & (df1['Item'] == item) & (df1['PricePointName'] == price_point)]['Size'].iloc[0]
+                    }
 
+                    # Make a prediction using your trained model
 
-# Item_Original
-# change places of number and valuies column with each other
-item_df = item_df[['1', '0']]
-# save df to csv
-item_df.to_csv('updated_Item_labelencoding.csv', index=False)
+                    prediction = model_ex.predict([list(row.values())])[0]
 
-# Load the label encoding CSV file into a dictionary object
-label_encoding = {}
-with open('updated_Item_labelencoding.csv', mode='r') as csv_file:
-    reader = csv.reader(csv_file)
+                    # Add the prediction to the DataFrame
+                    predictions_df = predictions_df.append({
+                        'Day': row['Day'],
+                        'Month': row['Month'],
+                        'Year': row['Year'],
+                        'Category': row['Category'],
+                        'Item': row['Item'],
+                        'PricePointName': row['PricePointName'],
+                        'Qty':prediction
+                    }, ignore_index=True)
 
-    for row in reader:
-        label_encoding[int(row[1])] = row[0]
+    # Check the predictions
+    # predictions_df['Day'].describe()
 
-# Convert the encoded values back to the original categorical values
-encoded_values = (predictions_df["Item"])
-original_values2 = [label_encoding[encoded_value] for encoded_value in encoded_values]
-
-
-# Price Point Name Original
-# change places of number and valuies column with each other
-Pricepointname_df = Pricepointname_df[['1', '0']]
-# save df to csv
-Pricepointname_df.to_csv('updated_size_labelencoding.csv', index=False)
+    category_df= pd.read_csv("updated_Category.csv")
 
 
-# Load the label encoding CSV file into a dictionary object
-label_encoding = {}
-with open('updated_size_labelencoding.csv', mode='r') as csv_file:
-    reader = csv.reader(csv_file)
-    for row in reader:
-        label_encoding[int(row[1])] = row[0]
-
-# Convert the encoded values back to the original categorical values
-encoded_values = (predictions_df["PricePointName"])
-original_values3 = [label_encoding[encoded_value] for encoded_value in encoded_values]
+    item_df = pd.read_csv("updated_Item_labelencoding.csv")
 
 
-# Create the dataframe
-final_df = pd.DataFrame({
-    'Day': predictions_df['Day'],
-    'Month': predictions_df['Month'],
-    'Year': predictions_df['Year'],
-    'Category': original_values1,
-    'Item': original_values2,
-    'Size': original_values3,
-    'Qty': predictions_df['Qty'],
-})
+    Pricepointname_df = pd.read_csv("updated_size_labelencoding.csv")
 
 
-if st.button("Predict"):
+    # convert predictions_df into dataFrame
+    predictions_df = pd.DataFrame(predictions_df)
+
+
+    # Category_Original 
+    # change places of number and valuies column with each other
+    # Load the label encoding CSV file into a dictionary object
+    label_encoding = {}
+    with open('updated_Category.csv', mode='r') as csv_file:
+        reader = csv.reader(csv_file)
+        for row in reader:
+            label_encoding[int(row[1])] = row[0]
+
+    # Convert the encoded values back to the original categorical values
+    encoded_values = (predictions_df["Category"])
+    original_values1 = [label_encoding[encoded_value] for encoded_value in encoded_values]
+
+
+    # Item_Original
+    # change places of number and valuies column with each other
+    item_df = item_df[['1', '0']]
+    # save df to csv
+    item_df.to_csv('updated_Item_labelencoding.csv', index=False)
+
+    # Load the label encoding CSV file into a dictionary object
+    label_encoding = {}
+    with open('updated_Item_labelencoding.csv', mode='r') as csv_file:
+        reader = csv.reader(csv_file)
+
+        for row in reader:
+            label_encoding[int(row[1])] = row[0]
+
+    # Convert the encoded values back to the original categorical values
+    encoded_values = (predictions_df["Item"])
+    original_values2 = [label_encoding[encoded_value] for encoded_value in encoded_values]
+
+
+    # Price Point Name Original
+    # change places of number and valuies column with each other
+    Pricepointname_df = Pricepointname_df[['1', '0']]
+    # save df to csv
+    Pricepointname_df.to_csv('updated_size_labelencoding.csv', index=False)
+
+
+    # Load the label encoding CSV file into a dictionary object
+    label_encoding = {}
+    with open('updated_size_labelencoding.csv', mode='r') as csv_file:
+        reader = csv.reader(csv_file)
+        for row in reader:
+            label_encoding[int(row[1])] = row[0]
+
+    # Convert the encoded values back to the original categorical values
+    encoded_values = (predictions_df["PricePointName"])
+    original_values3 = [label_encoding[encoded_value] for encoded_value in encoded_values]
+
+
+    # Create the dataframe
+    final_df = pd.DataFrame({
+        'Day': predictions_df['Day'],
+        'Month': predictions_df['Month'],
+        'Year': predictions_df['Year'],
+        'Category': original_values1,
+        'Item': original_values2,
+        'Size': original_values3,
+        'Qty': predictions_df['Qty'],
+    })
+
+
+if st.button("Show Results"):
 
 
     st.header("Prediction Result")
